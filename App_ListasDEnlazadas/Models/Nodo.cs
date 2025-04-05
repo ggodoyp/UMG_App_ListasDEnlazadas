7bf786a808db86938f19287fb33c6a74fc91a4d2
ggodoyp <ggodoyp@miumg.edu.gt>
2025-04-04 21:19:09 -0600

    Agregar archivos de proyecto.

---
+++ App_ListasDEnlazadas/Models/Nodo.cs
@@ -0,0 +1,19 @@
+﻿namespace App_ListasDEnlazadas.Models
+{
+    public class Nodo
+    {
+        public object Informacion { get; set; }
+        public Nodo? Referencia { get; set; }
+        public Nodo()
+        {
+            Informacion = string.Empty;
+            Referencia = null;
+        }
+
+        public Nodo(object informacion)
+        {
+            Informacion = informacion;
+            Referencia = null;
+        }
+    }
+}
